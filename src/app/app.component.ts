f5f1e297f82132c37c6f7b3b8ed4b283f262d966
Krishna0525 <116807294+Krishna0525@users.noreply.github.com>
2022-11-11 11:26:14 +0530

    Json file updated for empty values and recusrisive search functianality implimenting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-var json = require("./navigationLinks.json");
+const { htmlToText } = require('html-to-text');
+const htmlPages = require("./navigationLinks.json");
 
 @Component({
   selector: 'app-root',
@@ -19,71 +19,97 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient) { };
 
   searchNow() {
-    this.mainActivate = false;
-    this.searchActivate = true;
+    if (this.getSearchValue.trim().length > 2) {
+      this.mainActivate = false;
+      this.searchActivate = true;
+      this.checkStrInFiles(htmlPages);
+      return;
+
+      let levelOneLength = Object.keys(htmlPages).length;
+      if (levelOneLength > 0) {
+        for (let levelOne in htmlPages) {
+          const levelTwoNotArray = !Array.isArray(htmlPages[levelOne]);
+          if (levelTwoNotArray) {
+            const levelTwoLength = Object.keys(htmlPages[levelOne]).length;
+            if (levelTwoLength > 0) {
+              for (let levelTwo in htmlPages[levelOne]) {
+                const levelThreeNotArray = !Array.isArray(htmlPages[levelOne][levelTwo]);
+                if (levelThreeNotArray) {
+                  const levelThreeLength = Object.keys(htmlPages[levelOne][levelTwo]).length;
+                  if (levelThreeLength > 0) {
+                    for (let levelThree in htmlPages[levelOne][levelTwo]) {
+                      const levelFourNotArray = !Array.isArray(htmlPages[levelOne][levelTwo][levelThree]);
+                      if (levelFourNotArray) {
+
+                      } else {
+                        htmlPages[levelOne][levelTwo][levelThree].map((book: any) => {
+                          this.getHtmlFile(book.pageName)
+                        })
+                      }
+                      // console.log(htmlPages[levelOne][levelTwo][levelThree])
+                      // this.getHtmlFile(htmlPages[levelOne][levelTwo][levelThree])
+                    }
+                  }
+
+                } else {
+
+                }
+              }
+
+            }
+          } else {
+
+          }
+        }
+      }
+    } else {
+      alert("search string should min 2 chars")
+    }
   }
 
+  checkStrInFiles(listOfFiles: any) {
+    const objLength = Object.keys(listOfFiles).length;
+    if (objLength > 0) {
+      for (let page in listOfFiles) {
+        const notArray: boolean = !Array.isArray(listOfFiles[page]);
+        if (notArray) {
+          this.checkStrInFiles(listOfFiles[page])
+        } else {
+          listOfFiles[page].map((page: {}) => {
+            const objLength = Object.keys(page).length;
+            console.log(page)
+            const pageCheck: boolean = !Array.isArray(page);
+            // if(pageCheck) {
+            //   const objLength = Object.keys(listOfFiles).length;
+            //   this.checkStrInFiles(page)
+            // } else {
+
+            // }
+          })
+        }
+      }
+    }
+  }
 
   getHtmlFile(endPoint: any) {
     this.http.get('app/htmls/' + endPoint, {
       responseType: 'text'
     }).subscribe(
       (data) => {
-        this.htmlData += data.replace(/Company/gi, "<mark>Company</mark>");
-        // console.log(this.htmlData)
-      },
-      (error: any) => {
-        // this.htmlData = "error";
-        // console.log("error");
+        var re = new RegExp(this.getSearchValue, "gi");
+        // this.htmlData += data.replace(re, "<mark>" + this.getSearchValue + "</mark>");
+        const text = htmlToText(data, {
+          wordwrap: 300
+        })
+        let res = re.test(text);
+        if (res) {
+          console.log(text)
+        }
       }
-
     )
   }
 
   ngOnInit() {
-    let levelOneLength = Object.keys(json).length;
-    // Level One Length Check
-    if (levelOneLength > 0) {
-      // Looping through level one
-      for (let levelOne in json) {
-        // Checking level two is array or not
-        const levelTwoNotArray = !Array.isArray(json[levelOne]);
-        // If level two is not an array
-        if (levelTwoNotArray) {
-          // geting level two length
-          const levelTwoLength = Object.keys(json[levelOne]).length;
-          // Checking level two length grater then 0
-          if (levelTwoLength > 0) {
 
-            for (let levelTwo in json[levelOne]) {
-              const levelThreeNotArray = !Array.isArray(json[levelOne][levelTwo]);
-              if (levelThreeNotArray) {
-                const levelThreeLength = Object.keys(json[levelOne][levelTwo]).length;
-                if (levelThreeLength > 0) {
-                  for (let levelThree in json[levelOne][levelTwo]) {
-                    const levelFourNotArray = !Array.isArray(json[levelOne][levelTwo][levelThree]);
-                    if (levelFourNotArray) {
-
-                    } else {
-                      json[levelOne][levelTwo][levelThree].map((book: any) => {
-                        this.getHtmlFile(book.pageName)
-                      })
-                    }
-                    // console.log(json[levelOne][levelTwo][levelThree])
-                    // this.getHtmlFile(json[levelOne][levelTwo][levelThree])
-                  }
-                }
-
-              } else {
-
-              }
-            }
-
-          }
-        } else {
-
-        }
-      }
-    }
   }
 }
