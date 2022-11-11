18d457c7ef3c03b58dfc22e802799d60e5ee3b64
Krishna0525 <116807294+Krishna0525@users.noreply.github.com>
2022-11-11 18:17:32 +0530

    in Json file bredcrumbs property added to every array and search algo implemeted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 const { htmlToText } = require('html-to-text');
 const htmlPages = require("./navigationLinks.json");
 
@@ -8,108 +9,60 @@ const htmlPages = require("./navigationLinks.json");
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+
+
+export class AppComponent {
 
   title = 'authorative-litarature-test';
+
   htmlData: string = "";
   getSearchValue: string = "";
   mainActivate: boolean = true;
   searchActivate: boolean = false;
 
   constructor(private http: HttpClient) { };
+  searchResultArray: [] = []
 
-  searchNow() {
-    if (this.getSearchValue.trim().length > 2) {
-      this.mainActivate = false;
-      this.searchActivate = true;
-      this.checkStrInFiles(htmlPages);
-      return;
-
-      let levelOneLength = Object.keys(htmlPages).length;
-      if (levelOneLength > 0) {
-        for (let levelOne in htmlPages) {
-          const levelTwoNotArray = !Array.isArray(htmlPages[levelOne]);
-          if (levelTwoNotArray) {
-            const levelTwoLength = Object.keys(htmlPages[levelOne]).length;
-            if (levelTwoLength > 0) {
-              for (let levelTwo in htmlPages[levelOne]) {
-                const levelThreeNotArray = !Array.isArray(htmlPages[levelOne][levelTwo]);
-                if (levelThreeNotArray) {
-                  const levelThreeLength = Object.keys(htmlPages[levelOne][levelTwo]).length;
-                  if (levelThreeLength > 0) {
-                    for (let levelThree in htmlPages[levelOne][levelTwo]) {
-                      const levelFourNotArray = !Array.isArray(htmlPages[levelOne][levelTwo][levelThree]);
-                      if (levelFourNotArray) {
-
-                      } else {
-                        htmlPages[levelOne][levelTwo][levelThree].map((book: any) => {
-                          this.getHtmlFile(book.pageName)
-                        })
-                      }
-                      // console.log(htmlPages[levelOne][levelTwo][levelThree])
-                      // this.getHtmlFile(htmlPages[levelOne][levelTwo][levelThree])
-                    }
-                  }
-
-                } else {
-
-                }
-              }
-
-            }
-          } else {
-
-          }
-        }
-      }
-    } else {
-      alert("search string should min 2 chars")
-    }
-  }
-
-  checkStrInFiles(listOfFiles: any) {
+  checkStrInFiles(listOfFiles: any, searchStr: string) {
     const objLength = Object.keys(listOfFiles).length;
     if (objLength > 0) {
       for (let page in listOfFiles) {
         const notArray: boolean = !Array.isArray(listOfFiles[page]);
         if (notArray) {
-          this.checkStrInFiles(listOfFiles[page])
+          this.checkStrInFiles(listOfFiles[page], searchStr)
         } else {
-          listOfFiles[page].map((page: {}) => {
+          listOfFiles[page].map((pageIn: any) => {
             const objLength = Object.keys(page).length;
-            console.log(page)
-            const pageCheck: boolean = !Array.isArray(page);
-            // if(pageCheck) {
-            //   const objLength = Object.keys(listOfFiles).length;
-            //   this.checkStrInFiles(page)
-            // } else {
+            if (objLength > 0) {
+              if ("title" in pageIn && "pageName" in pageIn) {
+                this.http.get("app/htmls/" + pageIn.title, {
+                  responseType: 'text'
+                }).subscribe((data) => {
+                  const searchPattren = new RegExp(searchStr, "gi");
+                  const htmlToTextConvert = htmlToText(data, { wordwrap: 300 });
+                  const isSearchPattrenExist = searchPattren.test(htmlToTextConvert);
+                  if (isSearchPattrenExist) {
 
-            // }
-          })
+                  }
+                })
+              } else {
+                this.checkStrInFiles(pageIn, searchStr)
+              }
+            }
+          });
         }
       }
     }
   }
 
-  getHtmlFile(endPoint: any) {
-    this.http.get('app/htmls/' + endPoint, {
-      responseType: 'text'
-    }).subscribe(
-      (data) => {
-        var re = new RegExp(this.getSearchValue, "gi");
-        // this.htmlData += data.replace(re, "<mark>" + this.getSearchValue + "</mark>");
-        const text = htmlToText(data, {
-          wordwrap: 300
-        })
-        let res = re.test(text);
-        if (res) {
-          console.log(text)
-        }
-      }
-    )
+  searchNow() {
+    if (this.getSearchValue.trim().length > 2) {
+      this.mainActivate = false;
+      this.searchActivate = true;
+      this.checkStrInFiles(htmlPages, this.getSearchValue);
+    } else {
+      alert("search string should min 2 chars")
+    }
   }
 
-  ngOnInit() {
-
-  }
 }
